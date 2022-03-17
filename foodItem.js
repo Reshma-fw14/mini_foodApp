c1b8d483ab721ad86194cec523613b895c6f46af
Reshma-fw14 <reshmaresh9046@gmail.com>
2022-03-17 13:32:29 +0530

    initiated

---
+++ foodItem.js
@@ -0,0 +1,74 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Ambur Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 13,
+    img: 'images/biryani/Ambur-Chicken-Biryani.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Hyderabadi Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'images/biryani/Chicken-Biryani-hyd.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Egg Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 18,
+    img: 'images/biryani/egg-biryani.jpeg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Goan Fish Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'images/biryani/goan-fish-biryani.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Mutton Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 10,
+    img: 'images/biryani/hyd-Mutton-Biryani.jpg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Kamrupi Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 12,
+    img: 'images/biryani/kamrupi-biryani.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Kashmiri Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 13,
+    img: 'images/biryani/kashmiri.pulao.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Memoni Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 20,
+    img: 'images/biryani/memonibiryani.png',
+    quantity: 1
+},
+]
